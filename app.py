11b49bcdde7c7f46b619405a75450d5e2dddf9fc
nukin2da <134499652+nukin2da@users.noreply.github.com>
2023-06-01 13:49:51 +0900

    ver8.9

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ max_len = 100
 output_d_1_8 = 5 #자기의 모델에 맞는 output_d구하기 (지식요소 개수)
 c = cfg(vs=vs, emb=emb, hidden=hidden, nh=nh, device=device)
 
-model = RNNModel(output_d, c) #RNNModel 쓰는경우
+model_1_8 = RNNModel(output_d, c) #RNNModel 쓰는경우
 # model = LSTMModel(output_d, c) #LSTMModel 쓰는경우
 # model = ATTModel(output_d, c) #ATTModel 쓰는경우
 
